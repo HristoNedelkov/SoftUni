a7cf4be6968003bdd81a55d3d1fad5310b376e34
Icakiss <ico1989@abv.bg>
2020-10-04 12:34:48 +0300

    2 exercise

--- JS ADVANCED/DOM Manipulations ex/04. Sections/script.js
+++ JS ADVANCED/DOM Manipulations ex/04. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.querySelector('#content')
+
+   words.forEach(word => {
+      let div = document.createElement('div')
+      let p = document.createElement('p')
+      p.textContent = word
+      p.style.display = 'none'
+      div.appendChild(p)
+      content.appendChild(div)
+      div.addEventListener('click',() => {
+         p.style.display = 'block'
+      })
+
+   });
+
 }
--- JS ADVANCED/DOM Manipulations ex/07. Locked-Profile/app.js
+++ JS ADVANCED/DOM Manipulations ex/07. Locked-Profile/app.js
@@ -1,36 +1,21 @@
 function lockedProfile() {
+    document.querySelector("#main").addEventListener("click", handler);
+ 
+        console.log(document.querySelector("#main"))
 
-    let hiddenInfo1 = document.getElementById('user1HiddenFields')
-    let hiddenInfo2 = document.getElementById('user2HiddenFields')
-    let hiddenInfo3 = document.getElementById('user3HiddenFields')
-    let btns = document.getElementsByTagName('button')
-
-    let input = document.getElementsByName('user1Locked')[0]
-    let input2 = document.getElementsByName('user2Locked')[0]
-    let input3 = document.getElementsByName('user3Locked')[0]
-
-    let firstBtn = btns[0]
-    let secondBtn = btns[1]
-    let thirthBtn = btns[2]
-
-    function showMoreInfo(button, input, hidInfo) {
-        button.addEventListener('click', () => {
-            
-            if (input.checked == false) {
-                if (button.textContent == 'Show more') {
-                    hidInfo.style.display = 'block'
-                    button.textContent = 'Hide It'
-
-                } else {
-                    hidInfo.style.display = 'none'
-                    button.textContent = 'Show more'
-                }
-
+    function handler(e) {
+        console.log(e.target.parentNode.querySelector('input').checked);
+        if (e.target.type == "submit") {
+            const parent = e.target.parentNode;
+ 
+            let isLocked = parent.querySelector('input').checked;
+            if (!isLocked) {
+                let hiddenDiv = e.target.previousElementSibling;
+                
+                
+                hiddenDiv.style.display = hiddenDiv.style.display === 'block' ? 'none' : 'block';
+                e.target.textContent = e.target.textContent === 'Hide it' ? 'Show more' : 'Hide it';
             }
-        })
+        }
     }
-    showMoreInfo(firstBtn, input, hiddenInfo1)
-    showMoreInfo(secondBtn, input2, hiddenInfo2)
-    showMoreInfo(thirthBtn, input3, hiddenInfo3)
-
 }
