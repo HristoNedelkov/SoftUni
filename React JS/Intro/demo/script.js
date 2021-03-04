ac73ffcad4f402428adfe917daeb6d4cfd194a3d
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-03-04 12:43:18 +0200

    new module

---
+++ React JS/Intro/demo/script.js
@@ -0,0 +1,10 @@
+let root = document.getElementById('root');
+let jsx = (
+    <div>
+    <h1>Icko</h1>
+        <header>Gosho</header>
+        <footer>Tosho</footer>
+    </div>
+);
+
+ReactDOM.render(jsx, root);
---
+++ React JS/Intro/ickodemo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          <h1>I tova</h1>
+          <h2>Icko e goten</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
